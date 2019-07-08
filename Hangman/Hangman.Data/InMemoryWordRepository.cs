106c1376710b49d3c40eebe63c600eb7ee45bfc6
NaskoVasilev <nasko_vasilev@abv.bg>
2019-07-08 15:15:18 +0300

    Implement InMemoryWordRepository class for managing words

---
+++ Hangman/Hangman.Data/InMemoryWordRepository.cs
@@ -0,0 +1,11 @@
+﻿using Hangman.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hangman.Data
+{
+	public class InMemoryWordRepository : IWordRespository
+	{
+	}
+}
