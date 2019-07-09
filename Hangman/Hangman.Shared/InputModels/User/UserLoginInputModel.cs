b9e927f713413d4a041466765e9ce8dbcadc34e9
NaskoVasilev <nasko_vasilev@abv.bg>
2019-07-09 20:23:24 +0300

    User and Register Input Models

---
+++ Hangman/Hangman.Shared/InputModels/User/UserLoginInputModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hangman.Shared.InputModels.User
+{
+	public class UserLoginInputModel
+	{
+		public string Username { get; set; }
+
+		public string Password { get; set; }
+	}
+}
--- Hangman/Hangman.Shared/InputModels/User/UserRegisterInputModel.cs
+++ Hangman/Hangman.Shared/InputModels/User/UserRegisterInputModel.cs
@@ -1,10 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Hangman.Shared.InputModels.User
 {
 	public class UserRegisterInputModel
 	{
+		private const int UsernameMinLength = 5;
+		private const int PasswordMinLength = 5;
+
+		[Required]
+		[MinLength(UsernameMinLength)]
+		public string Username { get; set; }
+
+		[Required]
+		[EmailAddress]
+		public string Email { get; set; }
+
+		[Required]
+		[MinLength(PasswordMinLength)]
+		[Compare(nameof(ConfirmPassword)]
+		public string Password { get; set; }
+
+		public string ConfirmPassword { get; set; }
 	}
 }
