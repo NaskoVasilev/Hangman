6c25fffc6da1335dd6e4638f86e9f31b1980ac57
NaskoVasilev <nasko_vasilev@abv.bg>
2019-07-09 17:59:04 +0300

    Add all letter which matching the word

--- Hangman/Hangman.Logic/Game.cs
+++ Hangman/Hangman.Logic/Game.cs
@@ -15,5 +15,17 @@ namespace Hangman.Logic
 			CurrentWord = word;
 			PlayingWord = new string('_', word.Length);
 		}
+
+		public void AddMatchingLetters(string letter)
+		{
+			Console.WriteLine(letter);
+			for (int i = 0; i < CurrentWord.Length; i++)
+			{
+				if(CurrentWord[i].ToString() == letter)
+				{
+					PlayingWord = PlayingWord.Substring(0, i) + letter + PlayingWord.Substring(i + 1);
+				}
+			}
+		}
 	}
 }
