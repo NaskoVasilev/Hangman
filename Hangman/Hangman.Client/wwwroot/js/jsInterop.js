4b58ff4c0affd2319814e281e67033f8b3c52618
NaskoVasilev <nasko_vasilev@abv.bg>
2019-07-10 22:15:27 +0300

    js utility methods for managing auth token

---
+++ Hangman/Hangman.Client/wwwroot/js/jsInterop.js
@@ -0,0 +1,15 @@
+﻿const tokenKey = "auth_token";
+
+window.tokenManager = {
+    save: function (token) {
+        window.localStorage.setItem(tokenKey, token);
+        return true;
+    },
+    get: function (){
+        var token = window.localStorage.getItem(tokenKey);
+        return token;
+    },
+    remove: function () {
+        window.localStorage.removeItem(tokenKey);
+    }
+};
