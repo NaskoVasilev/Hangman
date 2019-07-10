3f72f74bd61c7767df78bf5ad1dfa373cc4a00a9
NaskoVasilev <nasko_vasilev@abv.bg>
2019-07-10 21:00:55 +0300

    Add new client-side blazor project Register and Login pages

---
+++ Hangman/Hangman.Client/Components/LoginComponent.cs
@@ -0,0 +1,15 @@
+﻿using Hangman.Shared.InputModels.User;
+using Microsoft.AspNetCore.Components;
+
+namespace Hangman.Client.Components
+{
+    public class LoginComponent : ComponentBase
+    {
+        public UserLoginInputModel InputModel { get; set; } = new UserLoginInputModel();
+
+        public void LoginUser()
+        {
+            System.Console.WriteLine(InputModel.Username + " " + InputModel.Password);
+        }
+    }
+}
---
+++ Hangman/Hangman.Client/Components/RegisterComponent.cs
@@ -0,0 +1,17 @@
+﻿using Hangman.Shared.InputModels.User;
+using Microsoft.AspNetCore.Components;
+using System;
+
+namespace Hangman.Client.Components
+{
+    public class RegisterComponent : ComponentBase
+    {
+        public UserRegisterInputModel InputModel { get; set; } = new UserRegisterInputModel();
+
+        public void RegisterUser()
+        {
+            Console.WriteLine(InputModel.Username + ' ' + InputModel.Email);
+            Console.WriteLine(InputModel.Password + ' ' + InputModel.ConfirmPassword);
+        }
+    }
+}
