6a8a23f97e67367dd2a43f61bedee06a986b595d
NaskoVasilev <nasko_vasilev@abv.bg>
2019-07-10 22:36:04 +0300

    ApiError and ApiResponse classes for more abstract communication between server and client

--- Hangman/Hangman.Client/Components/RegisterComponent.cs
+++ Hangman/Hangman.Client/Components/RegisterComponent.cs
@@ -1,15 +1,22 @@
-﻿using Hangman.Shared.InputModels.User;
+﻿using Hangman.Common;
+using Hangman.Shared.InputModels.User;
 using Microsoft.AspNetCore.Components;
 using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Hangman.Client.Components
 {
     public class RegisterComponent : ComponentBase
     {
+        [Inject]
+        public HttpClient HttpClient { get; set; }
+
         public UserRegisterInputModel InputModel { get; set; } = new UserRegisterInputModel();
 
-        public void RegisterUser()
+        public async Task RegisterUser()
         {
+            await HttpClient.PostJsonAsync(GlobalConstants.ApiBaseUrl + "user/register", InputModel);
             Console.WriteLine(InputModel.Username + ' ' + InputModel.Email);
             Console.WriteLine(InputModel.Password + ' ' + InputModel.ConfirmPassword);
         }
---
+++ Hangman/Hangman.Shared/ApiResponse.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Hangman.Shared
+{
+    public class ApiResponse<T>
+    {
+        public ApiResponse()
+        {
+        }
+
+        public ApiResponse(T data)
+        {
+            this.Data = data;
+        }
+
+        public ApiResponse(IEnumerable<ApiError> errors)
+        {
+            if (errors == null || !errors.Any())
+            {
+                this.Errors = new List<ApiError> { new ApiError("ApiResponse", "Unspecified error.") };
+            }
+            else
+            {
+                this.Errors = errors.ToList();
+            }
+        }
+
+        public ApiResponse(ApiError error)
+        {
+            this.Errors = new List<ApiError> { error };
+        }
+
+        public List<ApiError> Errors { get; set; }
+
+        public T Data { get; set; }
+
+        public bool IsOk => this.Errors == null || this.Errors.Count == 0;
+
+        public void AddError(ApiError error)
+        {
+            if(error == null)
+            {
+                return;
+            }
+
+            if(this.Errors == null)
+            {
+                this.Errors = new List<ApiError>();
+            }
+
+            this.Errors.Add(error);
+        }
+    }
+}
