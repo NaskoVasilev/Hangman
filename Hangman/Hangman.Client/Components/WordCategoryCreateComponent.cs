90d22866c69059e2a21c1e637be2b614be33db91
NaskoVasilev <nasko_vasilev@abv.bg>
2019-07-14 22:47:27 +0300

    Word Category create page and balzor component

---
+++ Hangman/Hangman.Client/Components/WordCategoryCreateComponent.cs
@@ -0,0 +1,28 @@
+﻿using Hangman.Shared;
+using Hangman.Shared.InputModels.WordCategory;
+using Hangman.Shared.ResponseModels.WordCategory;
+using System.Threading.Tasks;
+
+namespace Hangman.Client.Components
+{
+    public class WordCategoryCreateComponent : BaseHangmanComponent
+    {
+        public WordCategoryCreateComponent()
+        {
+            this.Model = new WordCategoryCreateInputModel();
+        }
+
+        public ApiResponse<WordCategoryResponseModel> Response { get; set; }
+
+        public WordCategoryCreateInputModel Model { get; set; }
+
+        public async Task CreateCategory()
+        {
+            Response= await ApiClient.CreateCategory(Model);
+            if(Response.IsOk)
+            {
+                UriHelper.NavigateTo("/");
+            }
+        }
+    }
+}
